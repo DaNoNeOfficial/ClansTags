36c2bc69af1a805b4a610f6a124a7135c84849a7
DaNoNeOfficial <daneonofficial@gmail.com>
2023-04-19 19:50:11 +1200

    updated

---
+++ Configs.cs
@@ -0,0 +1,33 @@
+﻿using Qurre.API.Addons;
+using Qurre.API.Attributes;
+using System.Collections.Generic;
+
+namespace ClansTags
+{
+    public static class Configs
+    {
+        internal static JsonConfig Config { get; private set; }
+        public static List<string> ClansTags { get; private set; }
+        public static bool EnableJoinMessage { get; private set; }
+        public static string JoinMessage { get; private set; }
+        public static ushort TimeMessageDisplay { get; private set; }
+        [EventMethod(4001u, 0)]
+        internal static void LoadReloadConfig()
+        {
+            if (Config == null)
+            {
+                Config = new JsonConfig("ClansTags");
+            }
+
+            ClansTags = Config.SafeGetValue("ClansTags", new List<string>()
+            {
+                "PLM",
+                "FFCL",
+                "KPB"
+            }, "Clan tags are available for installation");
+            EnableJoinMessage = Config.SafeGetValue("EnableJoinMessage", true, "Will greeting players with clan tags be included?");
+            JoinMessage = Config.SafeGetValue("JoinMessage", "<color=green>A member of the<color=red> <<%tag%>> </color>clan logged into the server!</color>", "A message at the player's entrance");
+            TimeMessageDisplay = Config.SafeGetValue("TimeMessageDisplay", (ushort) 10, "How long will the message be displayed?");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using ClansTags;
+using Qurre.API;
+using Qurre.API.Attributes;
+using Qurre.Events;
+using Qurre.Events.Structs;
+
+[PluginInit("ClansTags", "DaNoNe", "1.0.0")]
+static class Plugin
+{
+    [EventMethod(PlayerEvents.Join)]
+    static internal void JoinPlayerEvent(JoinEvent ev)
+    {
+        Player player = ev.Player;
+
+        foreach(string tag in Configs.ClansTags)
+        {
+            if (player.UserInfomation.Nickname.Contains($"[{tag}]"))
+            {
+                if(player.Administrative.RoleName == "")
+                {
+                    player.Administrative.RoleName = $"{tag}";
+                } else
+                {
+                    player.Administrative.RoleName = $"{tag} | {player.Administrative.RoleName}";
+                }
+                if (Configs.EnableJoinMessage)
+                {
+                    Map.Broadcast(Configs.JoinMessage.Replace("%tag%", tag), Configs.TimeMessageDisplay);
+                }
+            }
+        }
+    }
+}
